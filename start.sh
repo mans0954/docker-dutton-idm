2ef1621fcd4bc7e22f5440d6b073f47aeab0b8fd
Christopher Hoskin <mans0954@debian.org>
2017-11-03 07:08:52 +0000

    Get celery running (in the same container for now)

--- start.sh
+++ start.sh
@@ -8,4 +8,5 @@ done
 >&2 echo "Postgres is up - initialising"
 
 DATABASE_USER=postgres DATABASE_HOST=postgres-idm BROKER_SSL=no BROKER_HOSTNAME=rabbitmq-idm DJANGO_DEBUG=1 python3 manage.py migrate
+DJANGO_SETTINGS_MODULE=idm_core.settings DJANGO_DEBUG=1  celery -b rabbitmq-idm -B -A idm_core worker -l info &
 DATABASE_USER=postgres DATABASE_HOST=postgres-idm BROKER_SSL=no BROKER_HOSTNAME=rabbitmq-idm DJANGO_DEBUG=1 python3 manage.py runserver
